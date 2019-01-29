b9849c09822e5029987380fbc1edacb56c786e01
Gregor <gregor@martynus.net>
2019-01-28 21:19:54 -0800

    feat: initial version

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('work in progress lol')
+const { Toolkit } = require('actions-toolkit')
+const { context, github: { request } } = new Toolkit()
+
+const isWip = /\bwip\b/i.test(context.payload.pull_request.title)
+const newStatus = isWip ? 'pending' : 'success'
+
+// https://developer.github.com/v3/repos/statuses/#create-a-status
+request('POST /repos/:owner/:repo/statuses/:sha', context.repo({
+  sha: context.payload.pull_request.head.sha,
+  state: newStatus,
+  target_url: 'https://github.com/wip/action',
+  description: isWip ? 'work in progress' : 'ready for review',
+  context: 'WIP (action)'
+}))
